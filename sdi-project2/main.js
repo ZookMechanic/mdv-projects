3795c99c2e964274bb81b5f83fc8cd58ffdd6853
Kevin Flanagan <ZookMechanic@fullsail.edu>
2012-06-07 19:30:35 -0500

    Added my String Function and modded some other code.

--- sdi-project2/main.js
+++ sdi-project2/main.js
@@ -15,10 +15,17 @@ var requiredPoints = 750;
 var fullAmmo = false;
 var emptyAmmo = true;
 var zombies = 5;
+var weaponBoxPointReq = 950;
+var yourPoints = 7000;
+var minimumPoints = 2500;
+var weaponBox = ["HK21", "RPK", "Thundergun", "Gersh Device", "Commando", "Ray Gun", "AUG", "Galil"];
+var weapons = weaponBox[Math.floor(Math.random()*weaponBox.length)];
 
 
 // Procedure Function
 var openTopDoor = function (requiredPoints, myTotalPoints) {
+	var myTotalPoints = 4526;
+	var requiredPoints = 750;
 	if (myTotalPoints - requiredPoints >= requiredPoints) {
 		console.log("You have " + myTotalPoints + " points.  This door is only " + requiredPoints + " points.  Go ahead and open the door!");
 	} else {
@@ -73,8 +80,45 @@ var useTheBox = function () {};
 
 
 // Array Function
-var weaponBox = ["HK21", "RPK", "Thundergun", "Gersh Device", "Commando", "Ray Gun", "AUG", "Galil"];
-var weapons = weaponBox[Math.floor(Math.random()*weaponBox.length)];
-console.log("You hit up the box and received the " + weapons);
+var getNewWeapon = function () {
+	var weaponBox = ["HK21", "RPK", "Thundergun", "Gersh Device", "Commando", "Ray Gun", "AUG", "Galil"];
+	var weapons = weaponBox[Math.floor(Math.random()*weaponBox.length)];
+		console.log("You hit up the box and received the " + weapons + "!");
+};
+
+getNewWeapon();
+
+// String Function
+var endThisRound = function () {
+	var fullAmmo = true;
+	var zombies = 0;
+	if (fullAmmo === true || zombies === 0) {
+		console.log("You\'ve got a full clip of a good weapon hopefully...Kill that crawler and advance to the next round.");
+	} else {
+		console.log("Go and get anything else done that you need to.");
+	}
+};
+
+endThisRound();
+
+
+console.log("Just rinse and repeat this strategy and try not to get hit.");
+console.log("Remember your head and neck shots!");
+console.log("Ride the Lunar Landers if you have enough points to unlock the Power Room.");
+console.log("Once you get close to Round 10, most players will benefit from purchasing the Juggernaut Perk.");
+console.log("Next time we will continue on through to upgrading your weapons.");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
