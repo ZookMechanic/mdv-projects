f073efa520670a51fb19266e64f63746c019fe0d
Kevin Flanagan <ZookMechanic@fullsail.edu>
2012-07-20 23:47:16 -0500

    Added Method Function.

--- sdi-project3/main.js
+++ sdi-project3/main.js
@@ -13,7 +13,22 @@ var employee = {
 
 
 // Global Variables
+var currentTime = 630,
+	customers = 0;
 
 
 // Method Function
+var store = {
+	closeUp: function () {
+		if (currentTime === 630) {
+			console.log("Horray!  It\'s time to close this place down!");
+		} else if (customers === 0) {
+			console.log("All the customers are gone as well.");
+		} else {
+			console.log("We can\'t do anything until it\'s time and the customers are gone.");
+		}
+	}
+}; // End Method Function
 
+
+// 
