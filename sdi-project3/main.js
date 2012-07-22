d4008252f019c20db87c66c7049dbec007dc228b
Kevin Flanagan <ZookMechanic@fullsail.edu>
2012-07-22 16:11:09 -0500

    Added the Method Function.

--- sdi-project3/main.js
+++ sdi-project3/main.js
@@ -1,5 +1,5 @@
 // Kevin Flanagan
-// 18 July 2012
+// 22 July 2012
 // Deliverable 3
 // Closing Down The Shop
 
@@ -13,22 +13,35 @@ var employee = {
 
 
 // Global Variables
-var currentTime = 630,
-	customers = 0;
+
+
+// Method Procedure
+var store = {
+	closeUp: function (currentTime, currentCustomers) {
+		var currentTime = 630,
+			currentCustomers = 0;
+			if (currentTime === 630) {
+				console.log("Horray!  It\'s time to close this place down!");
+			} else if (currentCustomers === 0) {
+				console.log("All the customers are gone as well.");
+			} else {
+				console.log("We can\'t do anything until it\'s time and the customers are gone.");
+			}
+		}
+}; // End Method Procedure
 
 
 // Method Function
-var store = {
-	closeUp: function () {
-		if (currentTime === 630) {
-			console.log("Horray!  It\'s time to close this place down!");
-		} else if (customers === 0) {
-			console.log("All the customers are gone as well.");
+var preparation = {
+	floorCleanup: function () {
+		var carpetPieces = true,
+			greyCarpet = true;
+		if (carpetPieces === true && greyCarpet === true) {
+			console.log("Put the carpets out before moving the motorcycles in.");
 		} else {
-			console.log("We can\'t do anything until it\'s time and the customers are gone.");
+			console.log("We can\'t bring in the bikes until the carpets are out...It will ruin the floor.");
 		}
 	}
 }; // End Method Function
 
 
-// 
