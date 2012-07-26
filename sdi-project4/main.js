0471cba613fc8652bc19290169393bcfe15bdf96
Kevin Flanagan <ZookMechanic@fullsail.edu>
2012-07-26 00:25:39 -0500

    Added Stub Date Function.

--- sdi-project4/main.js
+++ sdi-project4/main.js
@@ -1,1 +1,16 @@
-alert("JavaScript works!");
+// Kevin Flanagan
+// 26 July 2012
+// Deliverable 4
+// My Library
+
+
+// Find the number of hours or days difference between two dates.
+today = new Date()
+var christmas = new Date(today.getFullYear(), 11, 25)
+if (today.getMonth()==11 && today.getDate()>25)
+christmas.setFullYear(christmas.getFullYear()+1)
+var one_day=1000*60*60*24
+
+console.log(Math.ceil((christmas.getTime()-today.getTime())/(one_day))+ " days left until Christmas!")
+
+
