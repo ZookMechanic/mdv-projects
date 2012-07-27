d76aa490cd0f57592f077de414f8b913a6026968
Kevin Flanagan <ZookMechanic@fullsail.edu>
2012-07-27 00:01:21 -0500

    Updated the date function.

--- sdi-project4/main.js
+++ sdi-project4/main.js
@@ -4,13 +4,27 @@
 // My Library
 
 
+// Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10.
+
+
+
+
+
 // Find the number of hours or days difference between two dates.
-today = new Date()
-var christmas = new Date(today.getFullYear(), 11, 25)
-if (today.getMonth()==11 && today.getDate()>25)
-christmas.setFullYear(christmas.getFullYear()+1)
-var one_day=1000*60*60*24
 
-console.log(Math.ceil((christmas.getTime()-today.getTime())/(one_day))+ " days left until Christmas!")
+// Difference In Date Function
+var date = function(yyyy, mm, dd) {
+	var today = new Date(); // Define current date
+	var dayOf = new Date (yyyy, (mm-1), dd); // Define second date
+	var minutes = (dayOf - today)/60000; // Subtracting both days to get minutes
+	var hours = minutes / 60; // Converting minutes to hours
+	var days = Math.floor(hours/24); // Converting hours to days
+	console.log("There are " + days + " days until my wife's birthday!");
+}; // End Difference In Date Function
 
 
+
+
+// Outputs
+
+date(2012, 12, 19);
