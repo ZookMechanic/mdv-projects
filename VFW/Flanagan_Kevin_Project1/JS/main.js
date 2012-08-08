44967e54a60ea973772398ddd9877d811757f47d
Kevin Flanagan <ZookMechanic@fullsail.edu>
2012-08-08 00:35:57 -0500

    Added first files to Project 2...refactored Project 1 HTML code...started adding JS functions.

---
+++ VFW/Flanagan_Kevin_Project1/JS/main.js
@@ -0,0 +1,28 @@
+// Kevin Flanagan
+// Web App Part 2
+// VFW 1208
+
+// Is the DOM ready?
+
+
+
+
+// getElementById Function
+
+
+
+
+
+// Variable defaults
+
+
+
+
+
+// Set Link & Submit Click Events
+
+
+
+
+
+
