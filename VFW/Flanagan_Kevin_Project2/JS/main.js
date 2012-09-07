ab3abb00ac524801f0c445b090a914ce7b4d9f76
Kevin Flanagan <ZookMechanic@fullsail.edu>
2012-09-06 23:20:27 -0500

    Added Select Field and Get Id Functions as well as some variables.

--- VFW/Flanagan_Kevin_Project2/JS/main.js
+++ VFW/Flanagan_Kevin_Project2/JS/main.js
@@ -2,3 +2,46 @@
 // Web App Part 2
 // VFW 1209
 
+// Wait until the DOM is ready.
+window.addEventListener("DOMContentLoaded", function() {
+
+
+	// getElementById Function
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+
+	
+	// Select Field Function
+	function makeCats() {
+		var formTag = document.getElementsByTagName("form");
+		selectLi = $('select'),
+		makeSelect = document.createElement('select');
+		makeSelect.setAttribute("id", "groups");
+		for(var i=0; j=contactGroups.length; i++) {
+			var makeOption = document.createElement('option');
+			var optText = contactGroups[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
+	}
+
+	
+	// Variable Defaults
+	var contactGroups = ["--Choose A Group--", "ATV", "Dirt Bike", "Watercraft", "Street Bike", "Cruiser", "Scooter"];
+	makeCats();
+
+
+	// Set Link & Submit Click Events
+	/*
+	var displayLink = $('displayLink');
+	displayLink.addEventListener("click", getData);
+	var clearLink = $('clear');
+	clearLink.addEventListener("click", clearLocal);
+	var save = $('submit');
+	save.addEventListener("click", storeData);
+	*/
+});
